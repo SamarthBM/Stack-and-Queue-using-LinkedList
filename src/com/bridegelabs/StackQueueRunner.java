4cbf730c0ab317548eb540249de7844264f310c9
SamarthBM <samarthbm18@gmail.com>
2021-07-17 12:24:23 +0530

    Initial Commit

---
+++ src/com/bridegelabs/StackQueueRunner.java
@@ -0,0 +1,18 @@
+/****************************************************************
+ * Purpose : To perform stack and queue using linked list.
+ * @author Samarth BM
+
+***************************************************************/
+
+package com.bridegelabs;
+
+
+public class StackQueueRunner {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
